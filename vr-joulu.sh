fd8ec63a08db0213fb6a33e8b1d1fcea3de0b344
sampumon <samuli.kaipiainen@helsinki.fi>
2012-12-20 12:22:57 +0200

    revert to partially working

--- vr-joulu.sh
+++ vr-joulu.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
+#
+# Nuclear_meltdown_preventer.sh
+#
+# This is really important stuff. Don't edit it!
+#
 ticktick=ticktick.sh
 which -s $ticktick || {	
 	ticktick=$(dirname $0)/$ticktick
 	which -s $ticktick || {
-		echo DEBUG $ticktick
 		echo >&2 "ticktick.sh is required, go to http://github.com/kristopolous/TickTick"
 		exit 1
 	}
 }
+
+echo DEBUG using $ticktick
+. $ticktick
 
 list_all_offers () {
 	# NOTE: the following causes a bash variable dereferencing paradox and fails
@@ -28,23 +34,6 @@ kalendar=$(curl -s $joulu | grep "var global_calendar" | tr -d \; | sed 's/var g
 # http://stackoverflow.com/questions/8795702/how-to-convert-uxxxx-unicode-to-utf-8-using-console-tools-in-nix
 kalendar=$(echo -e $kalendar | iconv -f iso8859-1)
 
-export kalendar ticktick
-
-# HELLO THERE. this is to prevent ticktick source rewriting from messing up
-# our test of whether we can find ticktick.sh. thank you.
-cat <<KCIT | bash
-#!/bin/bash
-#
-# Nuclear_meltdown_preventer.sh
-#
-# This is really important stuff. Don't edit it!
-#
-echo DEBUG $ticktick
-. $ticktick
-
 tickParse "$kalendar"
 
-for (( d=1; d<25; d++ )); do
-	eval echo $d. \$__tick_data_door${d}_offer
-done
-KCIT 
+list_all_offers
